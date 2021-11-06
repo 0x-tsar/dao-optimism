86987907377640b5b90bf044cdd69b0fbb52aaff
Gustavo Fernandes <contato.gustavofernandes@hotmail.com>
2021-11-06 00:26:47 -0300

    check

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,13 @@
+const DAO = artifacts.require("DAO");
+const DaoToken = artifacts.require("DaoToken");
+
 module.exports = async () => {
   try {
-    const [account] = await web3.eth.getAccounts();
+    const [account, account2, account3, account4, _] =
+      await web3.eth.getAccounts();
+    const daoToken = await DaoToken.deployed();
+    const dao = await DAO.deployed();
+
     console.log(account);
   } catch (error) {
     console.log(error);
